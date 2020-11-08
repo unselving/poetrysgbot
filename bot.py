0ecbf307c247c3f0031891ba94d716ae0b2c34b7
unselving <64175610+unselving@users.noreply.github.com>
2020-11-08 22:46:00 +0800

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,16 @@
+import tweepy
+
+from getquote import get_quote
+
+INTERVAL = 60 * 60 * 6  # tweet every 6 hours
+# INTERVAL = 15  # every 15 seconds, for testing
+
+auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
+auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
+api = tweepy.API(auth)
+
+while True:
+    print("about to get ad...")
+    ad = get_ad()
+    api.update_status(ad)
+    time.sleep(INTERVAL)
---
+++ charCheck.py
@@ -0,0 +1,21 @@
+import csv
+
+file1 = open("longlines.txt","w")
+
+with open('quotes.csv', mode='r', encoding='utf-8') as csv_file:
+    reader = csv.reader(csv_file)
+    line_count=0
+    longLines=0
+    for row in reader:
+        total = sum(len(i) for i in row)
+        if total > 278:
+            print(f'Line {line_count} is too long. It is {total} characters long.')
+            longLines+=1
+            file1.write(str(line_count)+"\n")
+            line_count+=1
+        else:
+            line_count+=1
+
+print(f'Processed {line_count} lines. {longLines} lines were too long.')
+   
+
