dab850c9f48516ee2d7f5d627136cda8a250e18d
unselving <64175610+unselving@users.noreply.github.com>
2020-11-09 15:06:46 +0800

    put access and consumer keys into heroku env

--- bot.py
+++ bot.py
@@ -1,7 +1,12 @@
   
 import tweepy
-
 from getquote import get_quote
+
+from os import environ
+CONSUMER_KEY = environ['CONSUMER_KEY']
+CONSUMER_SECRET = environ['CONSUMER_SECRET']
+ACCESS_KEY = environ['ACCESS_KEY']
+ACCESS_SECRET = environ['ACCESS_SECRET']
 
 INTERVAL = 60 * 60 * 6  # tweet every 6 hours
 # INTERVAL = 15  # every 15 seconds, for testing
